02e133c0404cbbc9be8dbf6e26d44c56fce72671
JorgeMioska <quispechilojorge@gmail.com>
2023-04-20 01:54:44 -0500

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Navbar } from "./components";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { HomePage, PokemonPage, SearchPage } from "./pages";
+
 const App = () => {
-  return <div>This is gonna be a typescript react Pokemon</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Navbar />}>
+        <Route index element={<HomePage />} />
+        <Route path="/pokemon/:id" element={<PokemonPage />} />
+        <Route path="/searchPokemon" element={<SearchPage />} />
+      </Route>
+      <Route path="/*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 };
 
 export default App;
