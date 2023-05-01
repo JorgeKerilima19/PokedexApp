7e71812b9db6cde703b664f4da9c6243f431d8f1
JorgeMioska <quispechilojorge@gmail.com>
2023-05-01 00:21:22 -0500

    pokemon search limited pokemon

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,21 +1,42 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useNavigate } from "react-router-dom";
+import { useContext } from "react";
 import { LogoPokedex } from "../helpers/Logos";
 import "../styles/navbar.css";
+import { PokedexContext } from "../context/PokedexContext";
 
 export const Navbar = () => {
+  const { valueSearch, onInputChange, onResetForm } =
+    useContext(PokedexContext);
+  const navigate = useNavigate();
+
+  const onSearchSubmit = (e: any) => {
+    e.preventDefault();
+    navigate("/searchPokemon", {
+      state: valueSearch,
+    });
+
+    onResetForm();
+  };
+
   return (
     <>
       <header className="pokedex__navbar">
         <Link to="/search" className="navbar__logo-container">
           <LogoPokedex />
         </Link>
-        <form className="navbar__form">
+        <form className="navbar__form" onSubmit={onSearchSubmit}>
           <input
             type="text"
             className="form__input"
             placeholder="Type something"
+            id=""
+            name="valueSearch"
+            value={valueSearch}
+            onChange={onInputChange}
           />
-          <button className="form__input-button">Search</button>
+          <button className="form__input-button" type="submit">
+            Search
+          </button>
         </form>
       </header>
       <main className="main">
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,3 +1,31 @@
+import { useLocation } from "react-router-dom";
+import { useContext } from "react";
+import { PokedexContext } from "../context/PokedexContext";
+
+import { CardPokemon } from "../components/CardPokemon";
+
+import "../styles/searchPage.css"
+
 export const SearchPage = () => {
-  return <div>SearchPage</div>;
+  const { limitedPokemon } = useContext(PokedexContext);
+  const location = useLocation();
+
+  const searchedPokemon = limitedPokemon.filter((pokemon: any) =>
+    pokemon.name.includes(location.state?.toLowerCase())
+  );
+
+  return (
+    <>
+      <div>
+        <p>
+          <span>{searchedPokemon.length}</span> results founded
+        </p>
+      </div>
+      <div className="pokedex__pokemon-container">
+        {searchedPokemon.map((pokemon: any) => {
+          return <CardPokemon pokemon={pokemon} key={pokemon.id} />;
+        })}
+      </div>
+    </>
+  );
 };
